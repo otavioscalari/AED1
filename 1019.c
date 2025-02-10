c3446cd0e4dd68fe4729e7b6c9aba08f0db7699f
otavioscalari <otavio.scalari@unifesp.br>
2025-02-10 20:32:33 -0300

    Create 1019.c

---
+++ 1019.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int N, x, hora, minuto, segundo;
+    x=3600;
+    
+    scanf("%d", &N);
+    hora = (N/x); 
+    minuto = (N -(x*hora))/60;
+    segundo = (N -(x*hora)-(minuto*60));
+    
+    printf("%d:%d:%d\n",hora,minuto,segundo);
+ 
+    return 0;
+}
